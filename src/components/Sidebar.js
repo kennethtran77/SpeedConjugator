f66cffd593f2500d91d4d2f86212c4a3b477227d
Kenneth Tran <kennethtran77@gmail.com>
2021-04-28 18:34:15 -0400

    Initial commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Sidebar.css';
+
+class Sidebar extends React.Component {
+    render() {
+        return (
+            <div className='sidebar'>
+                <h1>Sidebar</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Sidebar;
